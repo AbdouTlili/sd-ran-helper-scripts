bd32764d2baf97e2bfb81665cde974081d55c7e6
TliliAbdou <ha_tlili@esi.dz>
2022-03-02 09:45:57 +0100

    more visible logs in cleanK8s script

--- cleanK8sCluster.sh
+++ cleanK8sCluster.sh
@@ -1,2 +1,12 @@
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo "${RED}resetting k8s cluster ...${NC}"
 yes | sudo kubeadm reset 
+echo "${RED}resetting k8s cluster DONE !${NC}"
+
+echo "${RED}Cleaning iptables ...${NC}"
 sudo iptables -F
+
+echo "${RED}DONE ...${NC}"
