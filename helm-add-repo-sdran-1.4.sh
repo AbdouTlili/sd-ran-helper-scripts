76d5adce6fc4bf2c006e56df55940712d5fafcbd
AbdouTlili <ha_tlili@esi.dz>
2022-03-18 11:19:56 +0100

    updating for sd-ran 1.4.2

---
+++ helm-add-repo-sdran-1.4.sh
@@ -0,0 +1,5 @@
+helm repo add cord https://charts.opencord.org
+helm repo add atomix https://charts.atomix.io
+helm repo add onos https://charts.onosproject.org
+helm repo add sdran https://sdrancharts.onosproject.org
+helm repo update
--- sdran-setup-1.4.sh
+++ sdran-setup-1.4.sh
@@ -5,17 +5,17 @@ NC='\033[0m' # No Color
 echo "${RED}starting the installation of SDRAN requirements in kube-system ...${NC}"
 
 echo "${RED}installing atomix using helm ...${NC}"
-helm install atomix-controller atomix/atomix-controller -n kube-system --wait --version 0.6.8
+helm install atomix-controller atomix/atomix-controller -n kube-system --wait --version 0.6.9
 echo "${RED}Done installing atomix-controller ...${NC}"
 
 
 echo "${RED}installing atomix using helm ...${NC}"
-helm install atomix-raft-storage atomix/atomix-raft-storage -n kube-system --wait --version 0.1.15
+helm install atomix-raft-storage atomix/atomix-raft-storage -n kube-system --wait --version 0.1.25
 echo "${RED}Done installing atomix-raft-storage ...${NC}"
 
 
 echo "${RED}installing atomix using helm ...${NC}"
-helm install onos-operator onos/onos-operator -n kube-system --wait --version 0.4.14 
+helm install onos-operator onos/onos-operator -n kube-system --wait --version 0.5.2
 echo "${RED}Done installing onos-operator ...${NC}"
 
 echo
@@ -24,6 +24,6 @@ echo "${RED}deploying sdran chart ...${NC}"
 
 
 kubectl create ns sdran 
-helm -n sdran install sd-ran sdran/sd-ran --version 1.3.0
+helm -n sdran install sd-ran sdran/sd-ran --version 1.4.2
 echo
 echo "${RED} sdran setup completed.. \"run kubectl get pods -A\" to ensure that all pods are running correctly ${NC}"
