9b90f0d5ddb3e924f035b13f896a2e18e1a91020
AbdouTlili <ha_tlili@esi.dz>
2022-04-13 16:41:02 +0200

    using echo -e to enable colors in bash

--- cluster-clean.sh
+++ cluster-clean.sh
@@ -2,11 +2,11 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo "${RED}resetting k8s cluster ...${NC}"
+echo -e "${RED}resetting k8s cluster ...${NC}"
 yes | sudo kubeadm reset 
-echo "${RED}resetting k8s cluster DONE !${NC}"
+echo -e "${RED}resetting k8s cluster DONE !${NC}"
 
-echo "${RED}Cleaning iptables ...${NC}"
+echo -e "${RED}Cleaning iptables ...${NC}"
 sudo iptables -F
 
-echo "${RED}DONE ...${NC}"
+echo -e "${RED}DONE ...${NC}"
--- sdran-setup-1.4.sh
+++ sdran-setup-1.4.sh
@@ -2,28 +2,28 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo "${RED}starting the installation of SDRAN requirements in kube-system ...${NC}"
+echo -e "${RED}starting the installation of SDRAN requirements in kube-system ...${NC}"
 
-echo "${RED}installing atomix using helm ...${NC}"
+echo -e "${RED}installing atomix using helm ...${NC}"
 helm install atomix-controller atomix/atomix-controller -n kube-system --wait --version 0.6.9
-echo "${RED}Done installing atomix-controller ...${NC}"
+echo -e "${RED}Done installing atomix-controller ...${NC}"
 
 
-echo "${RED}installing atomix using helm ...${NC}"
+echo -e "${RED}installing atomix using helm ...${NC}"
 helm install atomix-raft-storage atomix/atomix-raft-storage -n kube-system --wait --version 0.1.25
-echo "${RED}Done installing atomix-raft-storage ...${NC}"
+echo -e "${RED}Done installing atomix-raft-storage ...${NC}"
 
 
-echo "${RED}installing atomix using helm ...${NC}"
+echo -e "${RED}installing atomix using helm ...${NC}"
 helm install onos-operator onos/onos-operator -n kube-system --wait --version 0.5.2
-echo "${RED}Done installing onos-operator ...${NC}"
+echo -e "${RED}Done installing onos-operator ...${NC}"
 
 echo
 echo
-echo "${RED}deploying sdran chart ...${NC}"
+echo -e "${RED}deploying sdran chart ...${NC}"
 
 
 kubectl create ns sdran 
 helm -n sdran install sd-ran sdran/sd-ran --version 1.4.2
 echo
-echo "${RED} sdran setup completed.. \"run kubectl get pods -A\" to ensure that all pods are running correctly ${NC}"
+echo -e "${RED} sdran setup completed.. \"run kubectl get pods -A\" to ensure that all pods are running correctly ${NC}"
