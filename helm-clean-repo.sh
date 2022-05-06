c8f43b479d999316dd59c2435eab4621550f45ac
AbdouTlili <ha_tlili@esi.dz>
2022-05-06 15:59:01 +0200

    commit form spoutnik

--- helm-clean-repo.sh
+++ helm-clean-repo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
+echo -e "${RED}starting the installation of SDRAN requirements in kube-system ...${NC}"
 helm repo remove atomix
 helm repo remove onos
 helm repo remove sdran
 helm repo remove cord
-
